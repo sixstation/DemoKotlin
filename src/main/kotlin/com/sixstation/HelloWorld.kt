a041238e0fd5eb114e514402fbeb0f0e7858b3b4
Six Luk <sixstation@me.com>
2020-07-15 22:59:26 +0800

    Initial commit

---
+++ src/main/kotlin/com/sixstation/HelloWorld.kt
@@ -0,0 +1,14 @@
+package com.sixstation
+
+fun main() {
+//    println("Hello World")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
